20a63e77318660414b0beb56c59ff42974bddbfc
BuildTools <unconfigured@null.spigotmc.org>
2024-08-03 22:29:43 +0200

    upd

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#menu');
-const menuBtn = document.querySelector('.btn-container');
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuBtn.classList.toggle('active');
-})
